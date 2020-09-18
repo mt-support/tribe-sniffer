5dbce45e32ef3d3a3943028bb5224354272df22e
András Guseo <andras@divi-magazine.com>
2020-09-18 10:07:36 +0200

    Adjusted exclude for local installation

--- tribe-sniffer.js
+++ tribe-sniffer.js
@@ -5,6 +5,7 @@
 // @description  Trying to find out what's running on a WordPress site with The Events Calendar.
 // @author       Andras Guseo
 // @include      https://*
+// @exclude      http*://*.local/wp-admin/*
 // @grant        none
 // ==/UserScript==
 
